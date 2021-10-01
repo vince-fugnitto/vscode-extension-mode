58e9f5bfd424bb147c4cdd46b92d7ab98415d247
vince-fugnitto <vincent.fugnitto@ericsson.com>
2021-10-01 08:09:57 -0400

    ext: update message to include additional info
    The commit updates the message when echoing the `ExtensionMode` to
    include additional information such as the extension name itself.
    
    Signed-off-by: vince-fugnitto <vincent.fugnitto@ericsson.com>

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,7 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
     let disposable = vscode.commands.registerCommand('vscode-extension-mode.echoMode', () => {
         const mode = vscode.ExtensionMode[context.extensionMode];
-        vscode.window.showInformationMessage(`'ExtensionMode' is '${mode}'`);
+        vscode.window.showInformationMessage(`vscode-extension-mode: { 'extensionMode': '${mode}' }`);
     });
     context.subscriptions.push(disposable);
 }
